ecfa68035c725825a9181236755fdf6d1188f539
Seth M <97685638+localseth@users.noreply.github.com>
2022-09-14 07:49:06 -0500

    add components

---
+++ src/components/Nav.js
@@ -0,0 +1,14 @@
+// stateless component
+import React from 'react';
+
+const Nav = () => (
+    <nav className="main-nav">
+        <ul>
+            <li><a href='#'>Cats</a></li>
+            <li><a href='#'>Dogs</a></li>
+            <li><a href='#'>Computers</a></li>
+        </ul>
+  </nav>
+);
+
+export default Nav;
