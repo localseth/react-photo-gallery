ad9d895fd1841f62d14fc8c946b1a90326276503
Seth M <97685638+localseth@users.noreply.github.com>
2022-09-15 09:38:47 -0500

    begin setup for data fetching

--- src/App.js
+++ src/App.js
@@ -10,9 +10,13 @@ import Nav from './components/Nav';
 import Grid from './components/Grid';
 
 function App() {
+
   return (
     <div className="container">
-      <Search apiKey={apiKey} />
+      <Search
+        apiKey={apiKey}
+        // fetchImages={fetchImages}
+      />
       <Nav />
       <Grid />
     </div>
